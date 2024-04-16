534138d9bb66ab5ff62d0e47f1cae97f66ce742f
hajjamn <hajjamn@gmail.com>
2024-04-16 12:42:47 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import axios from 'axios'
+
+export default {
+  components: {
+
+  },
+  data() {
+    return {
+      moviedbKey: '67894cbb91f10ee86602f7bd6b1f20c9',
+    }
+  }
+}
+</script>
+
+<template>
+
+  <div class="text-center">
+    {{ moviedbKey }}
+  </div>
+
+</template>
+
+<style lang="scss">
+@use 'bootstrap';
+</style>
