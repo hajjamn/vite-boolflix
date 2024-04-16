e1fe0db20d8f241d6b1ba1ea39badaccdfd996f8
hajjamn <hajjamn@gmail.com>
2024-04-16 13:06:22 +0200

    FIX: a placeholder for the searchbar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -14,8 +14,10 @@ export default {
 
   <div>
     <div class="mb-3">
-      <label for="exampleFormControlTextarea1" class="form-label">I am a SearchBar</label>
-      <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+      <label for="exampleFormControlTextarea1" class="form-label">I am a SearchBar
+        Label</label>
+      <textarea class="form-control" id="exampleFormControlTextarea1" rows="1"
+        placeholder="I am a searchbar"></textarea>
     </div>
   </div>
 
